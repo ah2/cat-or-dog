74a7c40147898dc2c1c6117423a7a9e3c46eb6ea
ah2 <44106895+ah2@users.noreply.github.com>
2023-12-31 21:02:55 +0400

    http requests ready, working on layout

---
+++ src/main/java/com/example/application/Http_tools.java
@@ -0,0 +1,131 @@
+package com.example.application;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Properties;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import com.example.application.views.main.MainView;
+
+public class Http_tools {
+	String key;
+	String url;
+
+	public Http_tools(String url, String key) {
+		this.url = url;
+		this.key = key;
+	}
+
+	public String get_img_url() {
+		String response = getImgJsonStr(url, key);
+		// System.out.println("got Json: " + response);
+		// Notification.show("got url: " + url);
+		
+		//System.out.println("url: " + url+" key: "+key);
+		ArrayList<String> urls;
+		if (response != null) {
+			urls= pars_to_url(response);
+			if (urls != null)
+				return pars_to_url(response).get(0);
+		}
+		// System.out.println("got url: " + url);
+
+		System.err.println("empty reponse!");
+		return null;
+	}
+
+	public static ArrayList<String> pars_to_url(String data) {
+		//System.out.println(data);
+		ArrayList<String> urls = new ArrayList<String>();
+		try {
+		JSONArray jArray = new JSONArray(data);
+		for (Object o : jArray) {
+			JSONObject jsonLineItem = (JSONObject) o;
+			String url = jsonLineItem.getString("url");
+			urls.add(url);
+		}
+		return urls;
+		}
+		catch(JSONException e) {
+			System.err.println("not a json array!");
+			return null;
+		}
+	}
+
+	public static String getImgJsonStr(String url, String api_key) {
+		try {
+			@SuppressWarnings("deprecation")
+			URL obj = new URL(url);
+			HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+			con.setRequestMethod("GET");
+			con.setRequestProperty("x-api-key", api_key);
+
+			int responseCode = con.getResponseCode();
+			if (responseCode == 200) {
+				BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+				String inputLine;
+				StringBuilder response = new StringBuilder();
+
+				while ((inputLine = in.readLine()) != null) {
+					response.append(inputLine);
+				}
+				in.close();
+
+				return response.toString();
+			}
+			System.err.println("Error response: " + responseCode + "!");
+			return null;
+		} catch (Exception e) {
+			System.err.println(e);
+			return null;
+		}
+
+	}
+	
+	
+	//example code outputs retrieved Url strings to console
+	public static void main(String[] args) {
+		Properties prop = new Properties();
+		try {
+			// load a properties file from class path, inside static method
+			prop.load(MainView.class.getClassLoader().getResourceAsStream("apikey.properties"));
+
+			// get the property value
+			String cat_key = prop.getProperty("API_CAT_KEY_VALUE");
+			String dog_key = prop.getProperty("API_DOG_KEY_VALUE");
+			String catURL = prop.getProperty("CAT_API_URL");
+			String dogURL = prop.getProperty("DOG_API_URL");
+			
+			
+			System.out.println("keys: "+cat_key + " - " + dog_key);
+			System.out.println("urls: "+catURL + " - " + dogURL);
+			
+			String cat_api_url = "https://api.thecatapi.com/v1/images/search?limit=1";
+	    	String dog_api_url = "https://api.thedogapi.com/v1/images/search?limit=1";
+	    	
+			Http_tools dogs = new Http_tools(dog_api_url, dog_key);
+			Http_tools cats = new Http_tools(cat_api_url, cat_key);
+
+			String cat = cats.get_img_url();
+			String dog = dogs.get_img_url();
+
+			if (cat != null && dog != null) {
+				System.out.println("received dog image url: " + cat);
+				System.out.println("received cat image url: " + dog);
+			}
+
+		} catch (IOException ex) {
+			ex.printStackTrace();
+		}
+
+	}
+
+}
--- src/main/java/com/example/application/views/main/MainView.java
+++ src/main/java/com/example/application/views/main/MainView.java
@@ -1,45 +1,120 @@
 package com.example.application.views.main;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Properties;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import com.example.application.Http_tools;
 import com.vaadin.flow.component.Key;
+import com.vaadin.flow.component.Unit;
 import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.html.Div;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.tabs.Tab;
+import com.vaadin.flow.component.tabs.Tabs;
+import com.vaadin.flow.component.tabs.TabsVariant;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
+import com.vaadin.flow.server.StreamResource;
 
 @PageTitle("Cat or Dog")
 @Route(value = "")
 public class MainView extends HorizontalLayout {
 
     private static final long serialVersionUID = 1L;
-    //@Value("${apiKey}")
-    //private String apiKey;
-	private TextField name;
-    private Button sayHello;
+
+    private String cat_key;
+    private String dog_key;
+    private String catURL;
+    private String dogURL;
 
     public MainView() {
     	
-    	//System.out.print("the key is:%s".format(apiKey));
-    	
-        name = new TextField("Your name");
-        sayHello = new Button("Say hello");
-        sayHello.addClickListener(e -> {
-            Notification.show("Hello " + name.getValue());
+    	if(cat_key == null || dog_key == null || catURL == null || dogURL == null)
+    		Get_keys();
+       
+        
+    	VerticalLayout main = new VerticalLayout();
+        add(main);
+        
+        Http_tools dogs = new Http_tools(dogURL, dog_key);
+		Http_tools cats = new Http_tools(catURL, cat_key);
+
+		String cat = cats.get_img_url();
+		String dog = dogs.get_img_url();
+
+		Image catImage = new Image();
+        if(cat!=null)
+        	catImage.setSrc(cats.get_img_url());
+        catImage.addClickListener(e -> {
+            Notification.show("you choose cat!");
+            catImage.setSrc(cats.get_img_url());
         });
-        sayHello.addClickShortcut(Key.ENTER);
-
-        setMargin(true);
-        setVerticalComponentAlignment(Alignment.END, name, sayHello);
-
-        add(name, sayHello);
+        //cat_dog_tab.add(catImage);
+        //add(catImage);
         
-        Image image = new Image();
-        //image.setSource(new ClassResource("/images/sponsor.png"));
+        Image dogImage = new Image();
+        if (dog != null)
+        	dogImage.setSrc(dogs.get_img_url());
+        dogImage.addClickListener(e -> {
+            Notification.show("you choose dog!");
+            dogImage.setSrc(dogs.get_img_url());
+        });
         
+        catImage.setWidth("30");
+        dogImage.setWidth("30");
+        
+        VerticalLayout vertL = new VerticalLayout();
+        vertL.add(dogImage);
+
+        VerticalLayout vertR = new VerticalLayout();
+        vertR.add(catImage);
+
+        
+        vertL.setWidth(10f, Unit.CM);
+        vertR.setWidth(10f, Unit.CM);
+        
+        HorizontalLayout layout = new HorizontalLayout(vertL, vertR);
+        main.add(layout);
+        //add(dogImage);
         
         
     }
+   
+    
+	private String Get_keys() {
+		Properties prop = new Properties();
+		try {
+		    //load a properties file from class path, inside static method
+		    prop.load(MainView.class.getClassLoader().getResourceAsStream("apikey.properties"));
 
+		    //get the property value
+		    cat_key = prop.getProperty("API_CAT_KEY_VALUE");
+		    dog_key = prop.getProperty("API_DOG_KEY_VALUE");
+		    catURL = prop.getProperty("CAT_API_URL");
+		    dogURL = prop.getProperty("DOG_API_URL");
+		    
+		    //	reading url from file is broken
+		    catURL = "https://api.thecatapi.com/v1/images/search?limit=1";
+		    dogURL = "https://api.thedogapi.com/v1/images/search?limit=1";
+	    	
+			
+		 
+		}
+		catch (IOException ex) {
+		    ex.printStackTrace();
+		}
+		return null;
+	}
 }
